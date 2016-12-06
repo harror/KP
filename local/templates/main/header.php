ff3733ae6415aa5223488d35a04a3a66c074cc09
Karpenko Alexander <harror15@gmail.com>
2016-12-07 01:15:01 +0300

    header changed

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -34,6 +34,22 @@
 </SCRIPT>
 <DIV id="wrap">
     <HEADER>
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:menu",
+            "",
+            Array(
+                "ALLOW_MULTI_SELECT" => "N",
+                "CHILD_MENU_TYPE" => "left",
+                "DELAY" => "N",
+                "MAX_LEVEL" => "3",
+                "MENU_CACHE_GET_VARS" => array(""),
+                "MENU_CACHE_TIME" => "3600",
+                "MENU_CACHE_TYPE" => "N",
+                "MENU_CACHE_USE_GROUPS" => "Y",
+                "ROOT_MENU_TYPE" => "left",
+                "USE_EXT" => "Y"
+            )
+        );?>
         <DIV class="common">
 
             <DIV class="phone">
